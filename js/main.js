593e1a44c0eac8b391b06da2542b02aa10bf366a
nezzie18 <cluafau@gmail.com>
2019-05-15 18:09:36 -0700

    Update.

--- js/main.js
+++ js/main.js
@@ -1,120 +1,87 @@
+
 // Parse the URL parameter
-function getParameterByName(name, url) {
+function getParameterByName (name, url) {
   if (!url) url = window.location.href;
-  name = name.replace(/[\[\]]/g, "\\$&");
-  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-    results = regex.exec(url);
+  name = name.replace(/[[\]]/g, '\\$&');
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)');
+  var results = regex.exec(url);
   if (!results) return null;
   if (!results[2]) return '';
-  return decodeURIComponent(results[2].replace(/\+/g, " "));
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
 
-function showContent() {
+function showContent () {
   var dynamicContent = getParameterByName('space');
 
-  if (dynamicContent == 'Beach') {
+  if (dynamicContent === 'Beach') {
     $('#beach-alt').show();
-  } else if (dynamicContent == 'City') {
+  } else if (dynamicContent === 'City') {
     $('#city-alt').show();
-  } else if (dynamicContent == 'Forest') {
+  } else if (dynamicContent === 'Forest') {
     $('#forest-alt').show();
-  } else if (dynamicContent == 'Library') {
+  } else if (dynamicContent === 'Library') {
     $('#library-alt').show();
-  } else if (dynamicContent == 'Cafe') {
+  } else if (dynamicContent === 'Cafe') {
     $('#cafe-alt').show();
-  } else if (dynamicContent == 'Park') {
+  } else if (dynamicContent === 'Park') {
     $('#park-alt').show();
-  } else if (dynamicContent == 'Mountains') {
+  } else if (dynamicContent === 'Mountains') {
     $('#mountains-alt').show();
-  } else if (dynamicContent == 'Rain') {
+  } else if (dynamicContent === 'Rain') {
     $('#rain-alt').show();
-  } else if (dynamicContent == 'Garden') {
+  } else if (dynamicContent === 'Garden') {
     $('#garden-alt').show();
   } else {
     $('#default-content').show();
   }
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
   showContent();
 });
 
-function appendParams() {
-  $("form").submit(function() {
+function appendParams () {
+  $('form').submit(function () {
     var space = getParameterByName('space');
     var alt = getParameterByName('alt');
 
-    var input_space = $("<input>")
-      .attr("type", "hidden")
-      .attr("name", "space").val(space);
+    var inputSpace = $('<input>')
+      .attr('type', 'hidden')
+      .attr('name', 'space').val(space);
 
-    var input_alt = "";
+    var inputAlt = '';
 
     if (alt) {
-      input_alt = $("<input>")
-        .attr("type", "hidden")
-        .attr("name", "alt").val(alt);
+      inputAlt = $('<input>')
+        .attr('type', 'hidden')
+        .attr('name', 'alt').val(alt);
     }
-    $(this).append(input_space);
-    $(this).append(input_alt);
+    $(this).append(inputSpace);
+    $(this).append(inputAlt);
   });
 }
 
-function removeParams() {
-  $('#home').submit(function() {
-    window.location.href = "http://localhost:3000/index.html?";
+function removeParams () {
+  $('#home').submit(function () {
+    window.location.href = 'http://localhost:3000/index.html?';
   });
   $('#alt').submit(function () {
-    window.location.href = "http://localhost:3000/index.html?"
+    window.location.href = 'http://localhost:3000/index.html?';
   });
   $('#setTime').submit(function () {
     var space = getParameterByName('space');
-    window.location.href = "http://localhost:3000/choose-alt.html?space=" + space;
-  })
+    window.location.href = 'http://localhost:3000/choose-alt.html?space=' + space;
+  });
 }
 
-function randomStudySpace() {
-  var spaces = ['Beach','City','Forest','Library','Cafe','Park','Mountains','Rain'];
-  var space = spaces[Math.floor(Math.random() * spaces.length)];
-  var alts = [];
-
-  if (space == 'Beach') {
-    alts = ['LowTide','HighTide'];
-  }
-  else if (space == 'City') {
-    alts = ['Day','Night'];
-  }
-  else if (space == 'Forest') {
-    alts = ['Creek','Waterfall'];
-  }
-  else if (space == 'Library') {
-    alts = ['FirstFloor','ThirdFloor'];
-  }
-  else if (space == 'Cafe') {
-    alts = ['Paris','NewYork'];
-  }
-  else if (space == 'Park') {
-    alts = ['Spring','Fall'];
-  }
-  else if (space == 'Mountains') {
-    alts = ['LowAltitude','HighAltitude'];
-  }
-  else if (space == 'Rain') {
-    alts = ['Light','Heavy'];
-  }
-
-  var alt = alts[Math.floor(Math.random() * alts.length)];
-
-  //window.location.href = "http://localhost:3000/set-timer.html?alt=" + alt + "&space=" + space;
-  return "http://localhost:3000/set-timer.html?alt=" + alt + "&space=" + space;
-
+function disableInput () {
+  $('#inputText').prop('disabled', true);
 }
 
-function disableInput() {
-  $("#inputText").prop('disabled', true);
-}
+function init () {
+  /* eslint-disable no-new */
+  /* eslint-disable new-cap */
 
-function init() {
   new appendParams();
   new removeParams();
   new disableInput();
